4bab781ef891b835170c9a98cc186a926a039cc2
kajaltushar/Arise <khase539@gmail.com>
2023-08-15 06:21:13 -0700

    ijh

---
+++ Myntra/src/main/java/Utility/Parametrization.java
@@ -0,0 +1,10 @@
+package Utility;
+
+public class Parametrization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Myntra/src/main/java/Utility/Scrrenshots.java
@@ -0,0 +1,10 @@
+package Utility;
+
+public class Scrrenshots {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
